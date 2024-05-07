1ba36b36206632d2b2ee506c6c8f530d451de976
dugga23 <164003474+dugga23@users.noreply.github.com>
2024-05-07 13:02:11 +0530

    Update login.js

--- controler/login.js
+++ controler/login.js
@@ -38,7 +38,7 @@ exports.login = async (req, res) => {
             message: 'Login successful',
             token,
             user: {
-                id: user._id,
+                _id: user._id,
                 username: user.username,
                 email: user.email,
                 firstname: user.firstname,
